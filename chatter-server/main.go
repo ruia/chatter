f4b13940580f179c6350a0bb2d6f488d3e8aaff1
Rui Almeida <rui.almeida011@gmail.com>
2022-05-25 02:13:43 +0100

    BUGs

--- chatter-server/main.go
+++ chatter-server/main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 const (
 	SERVER_HOST = "127.0.0.1"
 	SERVER_PORT = "2222"
 	SERVER_TYPE = "tcp"
+
+	WELCOME_MESSAGE = "Welcome to server CHATTER! Enjoy your stay %s"
 )
 
-// possible data structure?
 type ClientData struct {
-	address    string
 	name       string
 	connection net.Conn
 }
+
+var clients = make(map[string]*ClientData)
 
 func main() {
 	fmt.Println("Welcome to Chatter Server!")
@@ -30,10 +34,11 @@ func main() {
 		fmt.Println("Error msg:", err.Error())
 		os.Exit(1)
 	}
+
 	defer server.Close()
 
 	fmt.Printf("Serving on: Host: %v - Port: %v\n", SERVER_HOST, SERVER_PORT)
-	fmt.Println("Waiting for clients...")
+	fmt.Print("Waiting for clients...\n\n")
 
 	for {
 		connection, err := server.Accept()
@@ -41,20 +46,83 @@ func main() {
 			fmt.Println("Error accepting client!\n Error Msg: ", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("Client connected")
+
+		fmt.Println("Client connected.")
+
 		go processClient(connection)
 	}
 }
 
 func processClient(connection net.Conn) {
-	// create a map with connected clients?
-	//fmt.Println(connection.RemoteAddr())
+	defer func() {
+		fmt.Printf("%v: Disconnected\n", connection.RemoteAddr())
+		delete(clients, connection.RemoteAddr().String())
+		connection.Close()
+	}()
 
-	buffer := make([]byte, 1024)
-	mLen, err := connection.Read([]byte(buffer))
+	client := new(ClientData)
+	client.name = "anonymous"
+	client.connection = connection
+
+	rawDataName := getData(connection)
+	fmt.Printf("received: %v", rawDataName)
+	clientName := strings.Split(rawDataName, " ")
+	if clientName[0] == "/setName" && clientName[1] != "" {
+		client.name = strings.Trim(clientName[1], "\n")
+	}
+
+	// msg := fmt.Sprintf(WELCOME_MESSAGE, client.name)
+	println(client.name)
+	sendData(connection, "Welcome to server")
+
+	clients[client.connection.RemoteAddr().String()] = client
+
+	// reader := bufio.NewReader(connection)
+
+	for {
+		// userInput, err := reader.ReadString('\n')
+		userInput, err := bufio.NewReader(connection).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			return
+		}
+		userInput = strings.Trim(userInput, "\n")
+		// println(client.name)
+		// println(userInput)
+		// userInput := getData(connection)
+		// fmt.Println(clients[connection.RemoteAddr().String()].name)
+		println(client.name + ": " + userInput)
+		broadcast(*client, userInput)
+	}
+}
+
+func broadcast(currentClient ClientData, msg string) {
+	for k, cd := range clients {
+		if currentClient.connection.RemoteAddr().String() != k {
+			sendData(cd.connection, msg)
+		}
+	}
+}
+
+func getData(connection net.Conn) string {
+	data, err := bufio.NewReader(connection).ReadString('\n')
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
-	msg := string(buffer[:mLen])
-	fmt.Println("Received: ", msg)
+
+	return data
+}
+
+func sendData(connection net.Conn, data string) {
+	writer := bufio.NewWriter(connection)
+	_, err := writer.WriteString(data)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
