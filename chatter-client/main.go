9c76889bc02afde09538a8a9208baf3d312f7dc3
Rui Almeida <rui.almeida011@gmail.com>
2022-06-01 00:08:48 +0100

    bug fix's, still needs refactoring, more cleanup and new features

--- chatter-client/main.go
+++ chatter-client/main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"runtime"
+	"strings"
 )
 
 const (
 	SERVER_TYPE = "tcp"
 )
 
+var delimiter byte
+var delimiterFull string
+
 func main() {
-	// TODO later read from command line args?
+	oper := runtime.GOOS
+	switch oper {
+	case "windows":
+		delimiter = '\r'
+		delimiterFull = "\r\n"
+	default:
+		delimiter = '\n'
+		delimiterFull = "\n"
+	}
+
+	// TODO later read args from command line?
 	serverHost := "127.0.0.1"
 	serverPort := "2222"
 
 	fmt.Println("Welcome to Chatter Client!")
+
+	fmt.Println("Please enter your name: ")
+	name := ""
+	for name == "" {
+		name = readInput()
+	}
+
 	fmt.Println("Connecting to server...")
 
 	connection, err := net.Dial(SERVER_TYPE, serverHost+":"+serverPort)
@@ -23,15 +47,54 @@ func main() {
 	}
 
 	fmt.Printf("Connected to: %v:%v\n\n", serverHost, serverPort)
+	sendData(connection, "/setName "+name)
+
+	go handleDataFromServer(connection)
 
 	input := ""
 	for input != "/exit" {
-		fmt.Print("Input: ")
-		fmt.Scan(&input)
+		input = readInput()
 
 		if input != "/exit" {
-			_, err = connection.Write([]byte(input))
+			sendData(connection, input)
 		}
 	}
+}
 
+func handleDataFromServer(connection net.Conn) {
+	reader := bufio.NewReader(connection)
+	for {
+		data, err := reader.ReadString(delimiter)
+		data = strings.Trim(data, delimiterFull)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(">>", data)
+	}
+}
+
+func sendData(connection net.Conn, data string) {
+	writer := bufio.NewWriter(connection)
+
+	_, err := writer.WriteString(data)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func readInput() string {
+	input, err := bufio.NewReader(os.Stdin).ReadString(delimiter)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return input
 }
--- chatter-server/main.go
+++ chatter-server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"runtime"
 	"strings"
 )
 
@@ -13,7 +14,7 @@ const (
 	SERVER_PORT = "2222"
 	SERVER_TYPE = "tcp"
 
-	WELCOME_MESSAGE = "Welcome to server CHATTER! Enjoy your stay %s"
+	WELCOME_MESSAGE = "SERVER: Welcome to server CHATTER! Enjoy your stay %s"
 )
 
 type ClientData struct {
@@ -22,8 +23,20 @@ type ClientData struct {
 }
 
 var clients = make(map[string]*ClientData)
+var delimiter byte
+var delimiterFull string
 
 func main() {
+	oper := runtime.GOOS
+	switch oper {
+	case "windows":
+		delimiter = '\r'
+		delimiterFull = "\r\n"
+	default:
+		delimiter = '\n'
+		delimiterFull = "\n"
+	}
+
 	fmt.Println("Welcome to Chatter Server!")
 	fmt.Println("Starting the server...")
 
@@ -65,34 +78,21 @@ func processClient(connection net.Conn) {
 	client.connection = connection
 
 	rawDataName := getData(connection)
-	fmt.Printf("received: %v", rawDataName)
+	fmt.Printf("received: %v\n", rawDataName)
 	clientName := strings.Split(rawDataName, " ")
 	if clientName[0] == "/setName" && clientName[1] != "" {
-		client.name = strings.Trim(clientName[1], "\n")
+		client.name = clientName[1]
+		msg := fmt.Sprintf(WELCOME_MESSAGE, client.name)
+		sendData(connection, msg)
 	}
-
-	// msg := fmt.Sprintf(WELCOME_MESSAGE, client.name)
-	println(client.name)
-	sendData(connection, "Welcome to server")
 
 	clients[client.connection.RemoteAddr().String()] = client
 
-	// reader := bufio.NewReader(connection)
-
 	for {
-		// userInput, err := reader.ReadString('\n')
-		userInput, err := bufio.NewReader(connection).ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-			return
-		}
-		userInput = strings.Trim(userInput, "\n")
-		// println(client.name)
-		// println(userInput)
-		// userInput := getData(connection)
-		// fmt.Println(clients[connection.RemoteAddr().String()].name)
-		println(client.name + ": " + userInput)
-		broadcast(*client, userInput)
+		userInput := getData(connection)
+		msg := client.name + ": " + userInput
+		println(msg)
+		broadcast(*client, msg)
 	}
 }
 
@@ -105,7 +105,9 @@ func broadcast(currentClient ClientData, msg string) {
 }
 
 func getData(connection net.Conn) string {
-	data, err := bufio.NewReader(connection).ReadString('\n')
+	data, err := bufio.NewReader(connection).ReadString(delimiter)
+	data = strings.Trim(data, delimiterFull)
+
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
@@ -115,7 +117,7 @@ func getData(connection net.Conn) string {
 
 func sendData(connection net.Conn, data string) {
 	writer := bufio.NewWriter(connection)
-	_, err := writer.WriteString(data)
+	_, err := writer.WriteString(data + delimiterFull)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
